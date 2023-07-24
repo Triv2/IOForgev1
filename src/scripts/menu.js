d41e7db660a93d2d9c785f24646a70501a1b38b8
Trivial-lore <triviallore@gmail.com>
2023-07-24 14:14:32 -0500

    sticky and responsive navBar completed. Features, Products, and Prices need content to be added and responsiveness updated.

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -2,10 +2,15 @@ const menuButton=document.getElementById("menuButton");
 const menuItems=document.getElementById("navItems");
 
 menuButton.addEventListener("click", function(){
-  if (menuItems.style.display === "none") {
-    menuItems.style.display = "block";
+//   if (menuItems.style.visibility === "visible") {
+//     menuItems.style.visibility = "hidden";
+//   } else {
+//     menuItems.style.visibility = "visible";
+// } 
+  if (menuItems.style.display=== "none") {
+    menuItems.style.display = "flex";
   } else {
     menuItems.style.display = "none";
-}
+  }
 });
 
